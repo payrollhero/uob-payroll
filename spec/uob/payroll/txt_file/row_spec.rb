1446d05e099f577f54441bb74af0f31353ff52d1
Mathieu Jobin <mathieu@justbudget.com>
2021-08-30 16:23:54 +0900

    add more case types

--- spec/uob/payroll/txt_file/row_spec.rb
+++ spec/uob/payroll/txt_file/row_spec.rb
@@ -4,13 +4,14 @@ describe UOB::Payroll::TXTFile::Row do
 
   subject(:row) { described_class.new params }
   let(:string_version) { row.to_s }
+  let(:amount) { 43.35 }
 
   let(:params) {
     {
       bic_code: 'UOVBSGSG351',
       account_number: '3513220403',
       account_name: 'Juan de la Cruz',
-      amount: 43.35
+      amount: amount
     }
   }
 
@@ -19,17 +20,26 @@ describe UOB::Payroll::TXTFile::Row do
   it { expect(string_version).to eq expected_content }
 
   describe "formatted_amount" do
-    let(:params) {
-      {
-       bic_code: 'UOVBSGSG351',
-       account_number: '3513220403',
-       account_name: 'Juan de la Cruz',
-       amount: "2000.565"
-      }
-    }
+    describe "with an amount as a string param" do
+      let(:amount) { "2000.565" }
 
-    it "returns a proper value" do
-      expect(row.formatted_amount).to eq "0000000200057"
+      it "returns a proper value" do
+        expect(row.formatted_amount).to eq "0000000200057"
+      end
+    end
+    describe "with an amount as a float param" do
+      let(:amount) { 2000.565 }
+
+      it "returns a proper value" do
+        expect(row.formatted_amount).to eq "0000000200057"
+      end
+    end
+    describe "with an amount as a big decimal param" do
+      let(:amount) { BigDecimal("2000.565") }
+
+      it "returns a proper value" do
+        expect(row.formatted_amount).to eq "0000000200057"
+      end
     end
   end
 end
