19110489cecebf569eb19bd4cd3be7d5148844a2
Mathieu Jobin <mathieu@justbudget.com>
2021-08-30 16:14:18 +0900

    add new test to encounter the error from 2019

--- spec/uob/payroll/txt_file/row_spec.rb
+++ spec/uob/payroll/txt_file/row_spec.rb
@@ -17,4 +17,19 @@ describe UOB::Payroll::TXTFile::Row do
   let(:expected_content) { File.read 'spec/fixtures/row_sample.txt'}
 
   it { expect(string_version).to eq expected_content }
+
+  describe "formatted_amount" do
+    let(:params) {
+      {
+       bic_code: 'UOVBSGSG351',
+       account_number: '3513220403',
+       account_name: 'Juan de la Cruz',
+       amount: "2000.565"
+      }
+    }
+
+    it "returns a proper value" do
+      expect(row.formatted_amount).to eq "0000000200057"
+    end
+  end
 end
